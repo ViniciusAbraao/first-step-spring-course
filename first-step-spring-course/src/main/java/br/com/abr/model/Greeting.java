e8dc7053112f16160216d3ff9507b2673fcaad91
ViniciusAbraao <viniciusabraaocarvalho@gmail.com>
2025-08-22 20:27:55 -0300

    First steps in the course

---
+++ first-step-spring-course/src/main/java/br/com/abr/model/Greeting.java
@@ -0,0 +1,3 @@
+package br.com.abr.model;
+
+public record Greeting(Long id, String content) {}
---
+++ first-step-spring-course/src/test/java/br/com/abr/FirstStepSpringCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.abr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstStepSpringCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
